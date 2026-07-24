ba96dc2b9832d7ad3f0765ba212e3becf91fb45f
John Oldfield <john@bulletdigitalsolutions.co.uk>
2026-07-24 13:44:55 +0100

    Add Laravel 13 compatible test tooling and stop tracking composer.lock

---
+++ tests/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BulletDigitalSolutions\DoctrineEloquent\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+final class ExampleTest extends TestCase
+{
+    public function test_it_can_run(): void
+    {
+        $this->assertTrue(true);
+    }
+}
